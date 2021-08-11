d8e719ef61cd5198488d6202be9b8c42a53d270b
Valdor <example@gmai.com>
2021-08-11 14:47:58 +0300

    Конец первого занятия

---
+++ LessonMonitor/LessonMonitor.API/Controllers/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LessonMonitor.API.Controllers
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("MyUser")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public User[] Gitst()
+        {
+            var rnd = new Random();
+            var users = new User[10];
+            for (int i = 0; i < users.Length; i++)
+            {
+                var user = new User()
+                {
+                    UserName = "User_" + i.ToString()+";",
+                    Age=rnd.Next(18,75)
+                };
+                users[i] = user;
+            }
+            return users;
+        }
+
+        //[HttpGet]
+        //public string Get2()
+        //{
+        //    return "Vova";
+        //}
+    }
+}
